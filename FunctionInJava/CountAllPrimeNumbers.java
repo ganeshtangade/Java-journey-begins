f97e039b81842070313951553015f82f94d6d2a5
ganeshtangade <tangadeganesh02@gmail.com>
2025-12-23 15:57:53 +0530

    Print All Prime Number 2 to n

---
+++ FunctionInJava/CountAllPrimeNumbers.java
@@ -0,0 +1,33 @@
+package FunctionInJava;
+
+import java.util.Scanner;
+
+public class CountAllPrimeNumbers {
+public static boolean isPrime(int num){
+        if (num==2){
+            return true;
+        }
+        for(int i=2; i<=Math.sqrt(num);i++){
+            if (num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void PrimeInRange(int num){
+        for (int i=2; i<=num; i++){
+            if ( isPrime(i)){
+                System.out.println(i);
+            }
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter Number:");
+        int num=sc.nextInt();
+        PrimeInRange(num);
+}
+}
